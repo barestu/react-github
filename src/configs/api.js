cd25ede0c42a997cd9c5961c39fd6dcdcb18292d
Fandy Barestu <fandy@softwareseni.com>
2019-06-16 07:17:14 +0700

    Fix api constant

--- src/configs/api.js
+++ src/configs/api.js
@@ -6,6 +6,6 @@ export const GITHUB_CLIENT_ID = process.env.NODE_ENV !== 'production'
 
 export const GITHUB_CLIENT_SECRET = process.env.NODE_ENV !== 'production'
   ? process.env.REACT_APP_GITHUB_CLIENT_SECRET
-  : process.env.GITHUB_CLIENT_ID;
+  : process.env.GITHUB_CLIENT_SECRET;
 
 export default {};
